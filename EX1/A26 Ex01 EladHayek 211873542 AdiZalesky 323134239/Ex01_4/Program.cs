7c1fc7df5635c11eedac722e183612938fdfe2b4
eladh <elad3283@gmail.com>
2025-11-22 19:23:27 +0200

    created Ex01_4

---
+++ EX1/A26 Ex01 EladHayek 211873542 AdiZalesky 323134239/Ex01_4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ex01_4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string userInput;
+            do
+            {
+                Console.WriteLine("Please enter an alphanumeric string of exactly 10 characters:");
+                userInput = Console.ReadLine();
+
+            }
+            while (!StringUtilities.IsStringValid(userInput));
+
+            bool isPalindrome = StringUtilities.IsPalindromeRecursive(userInput);
+            Console.WriteLine("Is a palindrome: {0}", isPalindrome);
+
+            bool isAllNumbers = StringUtilities.IsAllNumbers(userInput);
+            bool isAllAlphabets = StringUtilities.IsAllAlphabets(userInput);
+            if (isAllNumbers)
+            {
+                bool canBeDevidedByThree = StringUtilities.CanBeDevidedByThree(userInput);
+                Console.WriteLine("Can be divided by 3: {0}", canBeDevidedByThree);
+            }
+            else if (isAllAlphabets)
+            {
+                int lowercaseCount = StringUtilities.GetLowercaseLettersCount(userInput);
+                bool isInAscendingOrder = StringUtilities.IsInAlphabeticalAscendingOrder(userInput);
+                Console.WriteLine("Number of lowercase letters: {0}", lowercaseCount);
+                Console.WriteLine("Is in alphabetical ascending order: {0}", isInAscendingOrder);
+            }
+
+        }
+    }
+
+}
---
+++ EX1/A26 Ex01 EladHayek 211873542 AdiZalesky 323134239/Ex01_4/StringUtilities.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Ex01_4
+{
+    public static class StringUtilities
+    {
+        public static bool IsStringValid(string i_String)
+        {
+            if (string.IsNullOrEmpty(i_String))
+            {
+                Console.WriteLine("Error: The string provided is null or empty");
+                return false;
+            }
+
+            if (i_String.Length != 10)
+            {
+                Console.WriteLine("Error: The string provided does not contain exactly 10 characters");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsAllNumbers(string i_String)
+        {
+            for (int i = 0; i < i_String.Length; i++)
+            {
+                if (!char.IsDigit(i_String[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool CanBeDevidedByThree(string i_String)
+        {
+            if (!IsAllNumbers(i_String))
+            {
+                Console.WriteLine("Error: The string provided is not all numbers");
+                return false;
+            }
+
+            int sumOfDigits = 0;
+            for (int i = 0; i < i_String.Length; i++)
+            {
+                sumOfDigits += (int)char.GetNumericValue(i_String[i]);
+            }
+
+            return sumOfDigits % 3 == 0;
+        }
+
+        public static bool IsAllAlphabets(string i_String)
+        {
+            for (int i = 0; i < i_String.Length; i++)
+            {
+                if (!char.IsLetter(i_String[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static int GetLowercaseLettersCount(string i_String)
+        {
+            if (!IsAllAlphabets(i_String))
+            {
+                Console.WriteLine("Error: The string provided is not all alphabets");
+                return -1;
+            }
+
+            int lowercaseCount = 0;
+            for (int i = 0; i < i_String.Length; i++)
+            {
+                if (char.IsLower(i_String[i]))
+                {
+                    lowercaseCount++;
+                }
+            }
+            return lowercaseCount;
+        }
+
+
+        public static bool IsInAlphabeticalAscendingOrder(string i_String)
+        {
+            if (!IsAllAlphabets(i_String))
+            {
+                Console.WriteLine("Error: The string provided is not all alphabets");
+                return false;
+            }
+
+            for (int i = 1; i < i_String.Length; i++)
+            {
+                if (char.ToLower(i_String[i]) < char.ToLower(i_String[i - 1]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsPalindromeRecursive(string i_String)
+        {
+            if (i_String.Length <= 1)
+            {
+                return true;
+            }
+
+            if (i_String[0] != i_String[i_String.Length - 1])
+            {
+                return false;
+            }
+            else
+            {
+                return IsPalindromeRecursive(i_String.Substring(1, i_String.Length - 2));
+            }
+        }
+    }
+}
