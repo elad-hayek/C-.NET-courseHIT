9817460fb8116d4ff0f8e524162115bcca1109a8
eladh <elad3283@gmail.com>
2025-11-23 20:30:01 +0200

    add ex5

---
+++ EX1/A26 Ex01 EladHayek 211873542 AdiZalesky 323134239/Ex01_5/NumberUtilities.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+
+namespace Ex01_5
+{
+    public static class NumberUtilities
+    {
+        public static bool IsNumberValid(string i_Number)
+        {
+            if (string.IsNullOrEmpty(i_Number) || i_Number.Length != 7)
+            {
+                Console.WriteLine("Error: The string provided is null or empty");
+                return false;
+            }
+
+            return int.TryParse(i_Number, out int number);
+        }
+
+        public static int HowManyDigitsAreGraterThanTheFirstDigit(string i_Number)
+        {
+            int digitsCount = 0;
+            if (!IsNumberValid(i_Number))
+            {
+                return -1;
+            }
+
+            int firstDigit = (int)char.GetNumericValue(i_Number[0]);
+            for (int i = 1; i < 7; i++)
+            {
+                int currentDigit = (int)char.GetNumericValue(i_Number[i]);
+                if (currentDigit > firstDigit)
+                {
+                    digitsCount++;
+                }
+            }
+
+            return digitsCount;
+        }
+
+        public static int HowManyDigitsDevideByThree(string i_Number)
+        {
+            int digitsCount = 0;
+            if (!IsNumberValid(i_Number))
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                int currentDigit = (int)char.GetNumericValue(i_Number[i]);
+                if (currentDigit % 3 == 0)
+                {
+                    digitsCount++;
+                }
+            }
+            return digitsCount;
+        }
+
+        public static int GetTheLargetsDiffrenceBetweenTwoDigits(string i_Number)
+        {
+            if (!IsNumberValid(i_Number))
+            {
+                return -1;
+            }
+
+            int maxDigit = (int)char.GetNumericValue(i_Number[0]);
+            int minDigit = (int)char.GetNumericValue(i_Number[0]);
+
+            for (int i = 1; i < 7; i++)
+            {
+                int currentDigit = (int)char.GetNumericValue(i_Number[i]);
+                if (currentDigit > maxDigit)
+                {
+                    maxDigit = currentDigit;
+                }
+                if (currentDigit < minDigit)
+                {
+                    minDigit = currentDigit;
+                }
+            }
+            return maxDigit - minDigit;
+        }
+
+        public static int GetTheMostCommonDigit(string i_Number, out int o_NumberOfAccurences)
+        {
+            if (!IsNumberValid(i_Number))
+            {
+                o_NumberOfAccurences = -1;
+                return -1;
+            }
+
+            o_NumberOfAccurences = 0;
+            int mostCommonDigit = -1;
+
+            for (int digit = 0; digit <= 9; digit++)
+            {
+                int currentDigitCount = 0;
+                for (int i = 0; i < 7; i++)
+                {
+                    int currentDigit = (int)char.GetNumericValue(i_Number[i]);
+                    if (currentDigit == digit)
+                    {
+                        currentDigitCount++;
+                    }
+                }
+                if (currentDigitCount > o_NumberOfAccurences)
+                {
+                    o_NumberOfAccurences = currentDigitCount;
+                    mostCommonDigit = digit;
+                }
+            }
+
+            return mostCommonDigit;
+
+        }
+    }
+}
---
+++ EX1/A26 Ex01 EladHayek 211873542 AdiZalesky 323134239/Ex01_5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Ex01_5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string userInput;
+            do
+            {
+                Console.WriteLine("Please enter a whole number exactly 7 digits:");
+                userInput = Console.ReadLine();
+
+            }
+            while (!NumberUtilities.IsNumberValid(userInput));
+
+            int graterThanFirstDigitCount = NumberUtilities.HowManyDigitsAreGraterThanTheFirstDigit(userInput);
+            Console.WriteLine(string.Format("Number of digits grater than the first digit: {0}", graterThanFirstDigitCount));
+
+            int digitsDividedByThreeCount = NumberUtilities.HowManyDigitsDevideByThree(userInput);
+            Console.WriteLine(string.Format("Number of digits that can be divided by 3: {0}", digitsDividedByThreeCount));
+
+            int largestDifference = NumberUtilities.GetTheLargetsDiffrenceBetweenTwoDigits(userInput);
+            Console.WriteLine(string.Format("The largest difference between two digits is: {0}", largestDifference));
+
+            int mostCommonDigit = NumberUtilities.GetTheMostCommonDigit(userInput, out int mostCommonDigitOccurrences);
+            Console.WriteLine(string.Format("The most common digit is: {0}, it appears {1} times", mostCommonDigit, mostCommonDigitOccurrences));
+        }
+    }
+}
